8c86053e5f8f01699becc5ac92938cbb51aa034d
WillYip716 <william.yip.716@gmail.com>
2020-06-06 17:38:14 -0700

    added new update methods

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -152,11 +152,65 @@ exports.genre_delete_post = function(req, res, next) {
 };
 
 // Display Genre update form on GET.
-exports.genre_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre update GET');
+exports.genre_update_get = function(req, res, next) {     
+  Genre.findById(req.params.id)
+      .exec(function (err, result) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.render('genre_form', { title: 'Genre Update', genre: result });
+      });
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre update POST');
-};
+exports.genre_update_post = [
+   
+  // Validate that the name field is not empty.
+  validator.body('name', 'Genre name required').trim().isLength({ min: 1 }),
+  
+  // Sanitize (escape) the name field.
+  validator.sanitizeBody('name').escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+
+    // Extract the validation errors from a request.
+    const errors = validator.validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    var genre = new Genre(
+      { name: req.body.name,
+        _id: req.params.id
+      }
+    );
+
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array()});
+      return;
+    }
+    else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      Genre.findOne({ 'name': req.body.name })
+        .exec( function(err, found_genre) {
+           if (err) { return next(err); }
+
+           if (found_genre) {
+             // Genre exists, redirect to its detail page.
+             res.redirect(found_genre.url);
+           }
+           else {
+
+             Genre.findByIdAndUpdate(req.params.id, genre, {}, function (err,result) {
+              if (err) { return next(err); }
+                // Successful - redirect to book detail page.
+                res.redirect(result.url);
+          });
+
+           }
+
+         });
+    }
+  }
+];
